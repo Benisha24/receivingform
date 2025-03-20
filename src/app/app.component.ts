8726c4678bf3db77724695bbef60d4b34c0ac7b6
Your Name <you@example.com>
2025-03-20 21:14:25 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReceivingformComponent } from './receivingform/receivingform.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReceivingformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
